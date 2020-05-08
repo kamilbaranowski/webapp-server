965cbbdb644abe51e1ca384971f4b0ed07aed06c
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-05-08 15:38:14 +0200

    Spring Security configuration

---
+++ src/main/java/pl/kamilbaranowski/chatappserver/configuration/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package pl.kamilbaranowski.chatappserver.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+/*
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(firebaseUserDetailsService);
+    }
+*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+    }
+
+   /* @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+*/
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        //TODO: Change password encoding (BCrypt)
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
